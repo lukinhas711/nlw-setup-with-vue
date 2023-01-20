1736cd0be69d6c07b9cd71dacfef4da49aa7d84a
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-01-20 16:53:33 -0300

    chore: move square colors of header to new component

---
+++ src/components/TheHeader/HeaderColors.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center gap-1">
+    <span class="w-3 h-3 rounded-sm border border-zinc-500 bg-zinc-800"></span>
+    <span class="w-3 h-3 rounded-sm border border-violet-700 bg-violet-700"></span>
+    <span class="w-3 h-3 rounded-sm border border-violet-600 bg-violet-600"></span>
+    <span class="w-3 h-3 rounded-sm border border-violet-500 bg-violet-500"></span>
+    <span class="w-3 h-3 rounded-sm border border-violet-400 bg-violet-400"></span>
+    <span class="w-3 h-3 rounded-sm border border-violet-300 bg-violet-300"></span>
+  </div>
+</template>
--- src/components/TheHeader/TheHeader.vue
+++ src/components/TheHeader/TheHeader.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { TheContainer } from '../TheContainer'
+import HeaderColors from './HeaderColors.vue'
 const add = () => {
   alert('adicionar um hábito')
 }
@@ -8,14 +9,7 @@ const add = () => {
 <template>
   <section class="flex justify-between items-center gap-4 min-w-[350px] mt-32 lg:min-w-[1024px]">
     <section class="flex flex-col gap-1">
-      <div class="flex items-center gap-1">
-        <span class="w-3 h-3 rounded-sm border border-zinc-500 bg-zinc-800"></span>
-        <span class="w-3 h-3 rounded-sm border border-violet-700 bg-violet-700"></span>
-        <span class="w-3 h-3 rounded-sm border border-violet-600 bg-violet-600"></span>
-        <span class="w-3 h-3 rounded-sm border border-violet-500 bg-violet-500"></span>
-        <span class="w-3 h-3 rounded-sm border border-violet-400 bg-violet-400"></span>
-        <span class="w-3 h-3 rounded-sm border border-violet-300 bg-violet-300"></span>
-      </div>
+      <HeaderColors />
       <h1 class="text-3xl font-black lg:text-4xl">
         habits
       </h1>
