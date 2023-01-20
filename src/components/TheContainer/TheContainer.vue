916c08eb4a125189dc4fc31850631a479a820966
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-01-20 13:51:53 -0300

    style: create header

--- src/components/TheContainer/TheContainer.vue
+++ src/components/TheContainer/TheContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto">
+  <div class="container m-auto">
     <slot />
   </div>
 </template>
--- src/components/TheHeader/TheHeader.vue
+++ src/components/TheHeader/TheHeader.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-  import { TheContainer } from '../TheContainer' 
+import { TheContainer } from '../TheContainer'
+const add = () => {
+  alert('adicionar um hábito')
+}
 </script>
 
 <template>
-  <TheContainer>
-    Header
-  </TheContainer>
+  <section class="flex justify-around items-center gap-4 min-w-[350px] mt-32 lg:min-w-[1024px]">
+    <section class="flex flex-col gap-1">
+      <div class="flex items-center gap-1">
+        <span class="w-3 h-3 rounded-sm border border-zinc-500 bg-zinc-800"></span>
+        <span class="w-3 h-3 rounded-sm border border-zinc-500 bg-violet-700"></span>
+        <span class="w-3 h-3 rounded-sm border border-zinc-500 bg-violet-600"></span>
+        <span class="w-3 h-3 rounded-sm border border-zinc-500 bg-violet-500"></span>
+        <span class="w-3 h-3 rounded-sm border border-zinc-500 bg-violet-400"></span>
+        <span class="w-3 h-3 rounded-sm border border-zinc-500 bg-violet-300"></span>
+      </div>
+      <h1 class="text-3xl font-black">
+        habits
+      </h1>
+    </section>
+    <section>
+      <button class="flex items-center justify-center gap-1 border border-violet-500 p-2 rounded-md" @click="add()">
+        <span class="text-2xl text-violet-500">+</span>Novo<span class="hidden lg:inline">hábito</span>
+      </button>
+    </section>
+  </section>
 </template>
